ae0ef1753fd869190159d57bb710f883961efb39
agcdtmr <112581827+agcdtmr@users.noreply.github.com>
2024-01-23 12:07:27 +0100

    Create whattoeat.py

---
+++ Python-Projects/whattoeat.py
@@ -0,0 +1,34 @@
+
+# Help me select who to contact
+
+
+import random
+
+ulam = [
+  "mash potato",
+  "bolognese",
+  "fritata",
+  "sinigang",
+  "chicken teriyaki",
+  "bulgur",
+  "egg + spinach",
+  "sopas",
+  "dumplings soup",
+  "pancake",
+  "mantis",
+  "yogurt sauce",
+  "courgette, carrot, celery soup",
+  "aubergine, sambal, tomato",
+  "creamy spinach, pasta",
+  "sushi burrito",
+  "menudo",
+  "pansit",
+  "chicken, ketchup manis, asin, oyster sauce"
+]
+
+# random.randint(1, 5) --> random number between 1 and 5
+# random.choice(array) --> random item in this array
+
+selected = random.choice(ulam).upper()
+
+print(f"You should cook {selected} today!\n")
